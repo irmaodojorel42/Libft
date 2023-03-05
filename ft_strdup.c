cee239f4f6c89b345f8f9fb8a0e356d04900c133
irmaodojorel42 <126424012+irmaodojorel42@users.noreply.github.com>
2023-03-05 14:57:22 +0000

    att6

--- ft_strdup.c
+++ ft_strdup.c
@@ -1,17 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/02 10:41:24 by marvin            #+#    #+#             */
-/*   Updated: 2023/03/02 10:41:24 by marvin           ###   ########.fr       */
+/*   Created: 2023/03/04 19:17:32 by marvin            #+#    #+#             */
+/*   Updated: 2023/03/04 19:17:32 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*#include "libft.h"
+#include "libft.h"
 
-void    *ft_memmove(void *dest, const void *src, size_t n)
+char    *ft_strdup(const char *s)
 {
+    char    *dup;
+
+    dup = malloc(ft_strlen(s) + 1);
+    if (dup == NULL)
+        return (NULL);
+    ft_memcpy(dup, s, ft_strlen(s) + 1);
+    return (dup);
+}
+
+/*int main(void)
+{
+    const char  *a = "sopa de galinha";
+    char    *result;
+
+    result = ft_strdup(a);
+    printf(":%s:\n", result);
+    return (0);
 }*/
--- libft.h
+++ libft.h
@@ -31,6 +31,7 @@ size_t  ft_strlen(const char *s);
 size_t  ft_strlcpy(char *dst, const char *src, size_t size);
 size_t  ft_strlcat(char *dst, const char *src, size_t size);
 
+char    *ft_strdup(const char *s);
 char    *ft_strchr(const char *s, int c);
 char    *ft_strrchr(const char *s, int c);
 char    *ft_strnstr(const char	*big, const char *little, size_t len);
